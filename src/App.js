70c569be6abe1110d31c233df3043e2b593d90bd
Lunaticsatoshi <deysarkersudeep@gmail.com>
2020-07-27 21:36:49 +0530

    Made components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MenuItem, FormControl, Select } from '@material-ui/core';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState(["USA", "UK", "France"]);
   return (
-    <div className="App">
-      <h1>My APP</h1>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 Dashboard</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="avc">
+            {countries.map(country => (
+              <MenuItem value={country}> {country} </MenuItem>
+            ))}
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            <MenuItem value="worldwide">Option 2</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
