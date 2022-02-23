e771218da94a1b3a2b4ebadefd01d4e2521b9e0f
Dare Rose <dhritipandey44@gmail.com>
2022-02-23 21:17:59 +0530

    deploying app

---
+++ build/precache-manifest.d6795064001815648b4247269bba30fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd58f7c529bd969bf73134ef2767b26e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db497669d7e9061f2c27",
+    "url": "/static/css/main.7a5abc43.chunk.css"
+  },
+  {
+    "revision": "ba071fc525047ac328ed",
+    "url": "/static/js/2.4cc16468.chunk.js"
+  },
+  {
+    "revision": "a52d103a09b0299b4cd97cfd81169642",
+    "url": "/static/js/2.4cc16468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db497669d7e9061f2c27",
+    "url": "/static/js/main.cd8268fb.chunk.js"
+  },
+  {
+    "revision": "2e075117383e7b156060",
+    "url": "/static/js/runtime-main.2f0bb968.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,59 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import { MenuItem, FormControl, Select } from '@material-ui/core';
+import React, { useState, useEffect } from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 import Cards from "./components/Cards/Cards";
 import Table from "./components/Table/Table";
 import Graph from "./components/Graph/Graph";
-import './App.css';
+import "./App.css";
 import { sortData } from "./utilities/util";
 //Material UI
-import { Card, CardContent, Typography } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
+import { Card, CardContent, Typography, Button } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 
 function App() {
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState(['worldwide']);
+  const [country, setCountry] = useState(["worldwide"]);
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
   const [casesType, setCasesType] = useState("cases");
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         setCountryInfo(data);
       });
-  }, [])
+  }, []);
   useEffect(() => {
     const getCountries = async () => {
       await fetch("https://disease.sh/v3/covid-19/countries")
         .then((response) => response.json())
         .then((data) => {
-          const countries = data.map((country) => (
-            {
-              name: country.country,
-              value: country.countryInfo.iso3, //FR AFG IN
-              flag: country.countryInfo.flag
-            }
-          ));
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso3, //FR AFG IN
+            flag: country.countryInfo.flag,
+          }));
           const sortedData = sortData(data);
 
           setCountries(countries);
           setTableData(sortedData);
         });
-    }
+    };
 
     getCountries();
   }, []);
 
   const onCountryChange = async (event) => {
     const countryCode = event.target.value;
-    const url = countryCode === "worldwide" ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${countryCode}`
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
     await fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setCountry(countryCode);
-        setCountryInfo(data)
-      })
-  }
+        setCountryInfo(data);
+      });
+  };
   console.log(countryInfo);
   return (
     <div className="app">
@@ -61,10 +62,17 @@ function App() {
         <div className="app__header">
           <h1>COVID-19 Dashboard</h1>
           <FormControl className="app__dropdown">
-            <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}
+            >
               <MenuItem value="worldwide">WorldWide</MenuItem>
-              {countries.map(country => (
-                <MenuItem value={country.value}><img src={country.flag} width="25px" height="15px"></img> <span className="country__name">{country.name}</span> </MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>
+                  <img src={country.flag} width="25px" height="15px"></img>{" "}
+                  <span className="country__name">{country.name}</span>{" "}
+                </MenuItem>
               ))}
               {/*
             <MenuItem value="worldwide">Option 2</MenuItem>
@@ -75,20 +83,38 @@ function App() {
         <div className="app__stats">
           <Grid container spacing={3}>
             <Grid item md={6} xs={12}>
-              <Cards className="infected" title="Total Cases" cases={countryInfo.todayCases} total={countryInfo.cases}></Cards>
+              <Cards
+                className="infected"
+                title="Total Cases"
+                cases={countryInfo.todayCases}
+                total={countryInfo.cases}
+              ></Cards>
             </Grid>
             <Grid item md={6} xs={12}>
-              <Cards className="recovered" title="Recovered Cases" cases={countryInfo.todayRecovered} total={countryInfo.recovered}></Cards>
+              <Cards
+                className="recovered"
+                title="Recovered Cases"
+                cases={countryInfo.todayRecovered}
+                total={countryInfo.recovered}
+              ></Cards>
             </Grid>
             <Grid item md={6} xs={12}>
-              <Cards className="deaths" title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths}></Cards>
+              <Cards
+                className="deaths"
+                title="Deaths"
+                cases={countryInfo.todayDeaths}
+                total={countryInfo.deaths}
+              ></Cards>
             </Grid>
             <Grid item md={6} xs={12}>
-              <Cards className="active" title="Active Cases" cases={countryInfo.active} total={countryInfo.active}></Cards>
+              <Cards
+                className="active"
+                title="Active Cases"
+                cases={countryInfo.active}
+                total={countryInfo.active}
+              ></Cards>
             </Grid>
           </Grid>
-
-
         </div>
         <div className="graph__container">
           <h3>Worldwide new {casesType}</h3>
@@ -104,6 +130,17 @@ function App() {
             </div>
           </CardContent>
         </Card>
+        <div
+          className="button"
+          onClick={() =>
+            window.open(
+              "https://darechan.github.io/covidsnaketracker/",
+              "_blank"
+            )
+          }
+        >
+          Play Game
+        </div>
       </div>
     </div>
   );
